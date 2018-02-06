6e5429ad9a9a8f2d9d5c8e4efea54d4146544409
Erik <enkiigm@gmail.com>
2018-02-06 14:33:52 +0100

    It lives!

--- test_boxofglory/main.lua
+++ test_boxofglory/main.lua
@@ -1,4 +1,7 @@
 
+local TheBox = require "TheBox"
+
+local bg = love.graphics.newImage("background.png")
 
 function love.load(arg)
   
@@ -6,10 +9,26 @@ end
 
 
 function love.update(dt)
+  local x, y = TheBox:tl()
   
+  x = x + math.random(-1, 1)
+  y = y + math.random(-1, 1)
+  
+  TheBox:tl(x, y)
 end
 
 
 function love.draw()
-  love.graphics.print("THEBOX!")
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(bg)
+  
+  love.graphics.setColor(TheBox.color)
+  love.graphics.polygon("fill", TheBox.pts)
+end
+
+
+function love.keypressed(key)
+  if key == "escape" then
+    love.event.quit()
+  end
 end
--- test_boxofglory/thebox.lua
+++ test_boxofglory/thebox.lua
@@ -1,5 +1,19 @@
 
 local TheBox = {}
+TheBox.color = {255, 0, 0}
+TheBox.pts   = {
+  300, 300,
+  1700, 400,
+  1600, 800,
+  200, 900,
+}
+
+function TheBox:tl(setx, sety)
+  self.pts[1] = setx or self.pts[1]
+  self.pts[2] = sety or self.pts[2]
+
+  return self.pts[1], self.pts[2]
+end
 
 
 
