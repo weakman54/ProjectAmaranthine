44088dec7152d32c92f14a3ff78b4366b7ca6495
Erik <enkiigm@gmail.com>
2018-04-10 17:06:11 +0200

    Removed load commands

--- test_VisualNovel2/scene1.lua
+++ test_VisualNovel2/scene1.lua
@@ -20,13 +20,10 @@ return {
   {"pauseExecution", elevatorDoorDuration}, -- Stops executing for an amount of time, this is useful for making sure a bunch of stuff doesn't happen while your tweens are running for example
 
 
-  --{"loadImage", "p2-bg", "assets/storyboards/p2-bg.png", {scale = {x = 3.23, y = 3.23}}},
+
   {"clear", "p2-bg"},
   {"pauseExecution", 2}, -- MAGIC NUMBER
 
-
-  --{"loadImage", "p3-bg", "assets/storyboards/p3-bg.png", {scale = {x = 3.23, y = 3.23}}},
-  --{"loadImage", "p3-s1", "assets/storyboards/p3-s1.png", {scale = {x = 3.23, y = 3.23}}},
 
   {"clear", "p1-bg"},
   {"show", "p3-bg"},
@@ -46,8 +43,6 @@ return {
 
 
 
-  --{"loadImage", "p4-bg", "assets/storyboards/p4-bg.png", {scale = {x = 3.23, y = 3.23}}},
-  --{"loadImage", "p4-s1", "assets/storyboards/p4-s1.png", {scale = {x = 3.23, y = 3.23}}},
 
   {"clear", "p4-bg"},
   {"show", "p4-s1"},
@@ -55,8 +50,6 @@ return {
   {"pauseExecution", 2},
 
 
-  --{"loadImage", "p5-bg", "assets/storyboards/p5-bg.png", {scale = {x = 3.23, y = 3.23}}},
-  --{"loadImage", "p5-s1", "assets/storyboards/p5-s1.png", {scale = {x = 3.23, y = 3.23}}},
 
   {"clear", "p5-bg"},
   {"show", "p5-s1"},
@@ -65,27 +58,23 @@ return {
   {"pauseExecution", 2},
 
 
-  --{"loadImage", "p6-bg", "assets/storyboards/p6-bg.png", {scale = {x = 3.23, y = 3.23}}},
 
   {"clear", "p6-bg"},
   {"pauseExecution", 2},
 
 
-  --{"loadImage", "p7-bg", "assets/storyboards/p7-bg.png", {scale = {x = 3.23, y = 3.23}}},
 
   {"clear", "p7-bg"},
   {"addText", "t", "That thing she is saying here...", {pos = {x = 0, y = 1080 - 200}, color = {000, 000, 000}, width = 1920, align = "center"} },
   {"waitForInput"},
 
 
-  --{"loadImage", "p8-bg", "assets/storyboards/p8-bg.png", {scale = {x = 3.23, y = 3.23}}},
 
   {"clear", "p8-bg"},
   {"addText", "t", "The other thing she is saying there...", {pos = {x = 0, y = 1080 - 200}, color = {000, 000, 000}, width = 1920, align = "center"} },
   {"waitForInput"},
 
 
-  --{"loadImage", "p9-bg", "assets/storyboards/p9-bg.png", {scale = {x = 3.23, y = 3.23}}},
 
   {"clear", "p9-bg"},
   {"addText", "t", "wooah!", {pos = {x = 0, y = 200}, color = {000, 000, 000}, width = 1920, align = "center"} },
@@ -322,9 +311,5 @@ return {
   
   
   
-
-  --{"loadImage", "endMarker", "assets/storyboards/endMarker.png", {scale = {x = 3.23, y = 3.23}}},
   {"clear", "endMarker"},
-
-
 }
