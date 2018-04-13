010f8d5360e830e1643b7435bc51053341fa1bf0
Erik <enkiigm@gmail.com>
2018-04-13 14:30:19 +0200

    Imported Input library, * refactored old "input" code

--- Ever_End_alpha/main.lua
+++ Ever_End_alpha/main.lua
@@ -5,10 +5,30 @@ local Gamestate = require "hump.gamestate"
 
 -- States
 local stateMain = require "gamestates.stateMain"
+local baton = require "baton.baton"
 
 
-
+-- Other stuff:
 local scale = {x = 1, y = 1} -- scale hack
+
+input = baton.new { -- Should be global
+  controls = {
+    left   = {'key:left' , "key:a", 'axis:leftx-', 'button:dpleft'},
+    right  = {'key:right', "key:d", 'axis:leftx+', 'button:dpright'},
+    up     = {'key:up'   , "key:w", 'axis:lefty-', 'button:dpup'},
+    down   = {'key:down' , "key:s", 'axis:lefty+', 'button:dpdown'},
+    attack = {'key:space',                         'button:a'},
+    guard  = {"key:g"    ,                         "button:rightshoulder", "axis:triggerright+"},
+    dodge  = {"key:d"    ,                         "button:x"},
+    heal   = {"key:h"    ,                         "button:y"},
+
+    systemStart = {"key:escape",                   "button:start"},                      
+  },
+  pairs = {
+    move = {'left', 'right', 'up', 'down'}
+  },
+  joystick = love.joystick.getJoysticks()[1],
+}
 
 
 
@@ -34,33 +54,27 @@ end
 
 
 function love.update(dt)
+  input:update()
+
+  if input:pressed("systemStart") then love.event.quit() end
+  
+
   Gamestate.update(dt)
 end
 
 
 function love.draw()
   love.graphics.scale(scale.x, scale.y) -- Scale hack
-  
+
   Gamestate.draw()
 end
 
 
 function love.keypressed(key, scancode, isrepeat)
-
+  -- NOTE: cannot use input library in keypressed! use it in update instead!
   if scancode == "`" then
     love._openConsole()
   end
-  --
-
-  if key == "escape" then
-    love.event.quit()
-
-  elseif key == "" then
-
-  end
-  --
-
-  Gamestate.keypressed(key, scancode, isrepeat)
 end
 
 
