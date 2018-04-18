3eca10018ff990f1a705fa4d9b849cc2ac5d5bbe
Unknown <sebwallin@hotmail.com>
2018-04-18 11:02:52 +0200

    animationComment

---
+++ test_battlesystem_wAB/loadAnimation.lua
@@ -0,0 +1,5 @@
+--loadAnimation 
+
+--looks at: Targets name (Quit, Player, etc.) , state name, class name and name, stance, numer, and takes in all files with that naming convention from the correct directiory
+--or
+--looks at: files in "Collection", separate the names in the naming convention, find the files and load them 
