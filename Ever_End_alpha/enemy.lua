a5d0c07ceeca5a7c480e0b2891af1179c71ce875
Erik <enkiigm@gmail.com>
2018-05-02 09:38:58 +0200

    Enemy combo test

--- Ever_End_alpha/enemy.lua
+++ Ever_End_alpha/enemy.lua
@@ -130,13 +130,20 @@ function enemy:initializeAttacks()
   self.attackWeights = {} -- NOTE: these are hardcoded atm, should make sure to fix a better way of loading the weights
 
   self.attacks["high_attack01"] = self:loadAttack({name = "high_attack01", animName = "high_attack01", damage = 1, stance = "high"}, 30)
-  self.attackWeights["high_attack01"] = 1
+  self.attackWeights["high_attack01"] = 2/7
 
   self.attacks["low_attack01"]  = self:loadAttack({name = "low_attack01" , animName = "low_attack01" , damage = 4, stance = "low" }, 30)
-  self.attackWeights["low_attack01"] = 1
+  self.attackWeights["low_attack01"] = 2/7
 
 
-  self.attackWeights["taunt"] = 0.5 -- Feels kindof misplaced but eh...
+
+  self.attacks["comboTest"]  = self:loadAttack({name = "comboTest" , animName = "low_attack01" , damage = 4, stance = "low"}, 30)
+  self.attacks["comboTest"].nextAttack = "low_attack01"
+  self.attackWeights["comboTest"] = 2/7
+
+
+
+  self.attackWeights["taunt"] = 1/7 -- Feels kindof misplaced but eh...
 
 
   self.counterAttack = self:loadAttack({name = "counter_attack", animName = "counter_attack", damage = 2, stance = "high"}, 30)
@@ -239,7 +246,7 @@ function enemy:initializeSM()
   --
 
   sm:add("offensive", {
-      enter = function(self, kind)
+      enter = function(self, kind, nextAttack)
         enemy.dbg_trigger_offensive_action = false
 
 
@@ -248,9 +255,16 @@ function enemy:initializeSM()
 --        local attackI = math.random(#enemy.attacks * 2 + 1)
 --        attackI = math.ceil(attackI/2)
 
-        if kind and kind == "counterAttack" then
-          self.curAttack = enemy.counterAttack
-          enemy.counterWeightTable = {guard = enemy.baseGuardWeight, counterAttack = 1} -- Reset weights for counter attack
+        if kind then
+          if kind == "counterAttack" then
+            self.curAttack = enemy.counterAttack
+            enemy.counterWeightTable = {guard = enemy.baseGuardWeight, counterAttack = 1} -- Reset weights for counter attack
+
+          elseif kind == "combo" then
+
+            self.curAttack = enemy.attacks[nextAttack]
+
+          end
 
         else
           local choice = lume.weightedchoice(enemy.attackWeights)
@@ -260,6 +274,10 @@ function enemy:initializeSM()
 
           self.curAttack = enemy.attacks[choice]
         end
+
+--        if self.curAttack.nextAttack then
+--          print("COMBO!")
+--        end
 
 
         -- TODO: set animation
@@ -287,7 +305,11 @@ function enemy:initializeSM()
         if ac:curEvent() == "ended" then
           -- TODO: if nextAttack, switch back to offensive with the info (sm:switch("offensive", data))
           -- TODO: switch to correct state (combo?)
-          sm:switch("idle")
+          if self.curAttack.nextAttack then
+            return sm:switch("offensive", "combo", self.curAttack.nextAttack)
+          else
+            return sm:switch("idle")
+          end
         end
 
         enemy.timingStage = 0
