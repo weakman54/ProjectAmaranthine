c8561c0464d87aa2d760bad158e3cffab09686a5
Erik <enkiigm@gmail.com>
2018-05-07 11:40:41 +0200

    Added dumpToFile function to utils

--- Ever_End_alpha/util.lua
+++ Ever_End_alpha/util.lua
@@ -13,7 +13,7 @@ end
 
 function filterArray(array, filterF)
   local out = {}
-  
+
   for i, v in ipairs(array) do
     if filterF(array, i, v) then
       table.insert(out, v)
@@ -33,4 +33,63 @@ function countAll(table)
     s = s + 1
   end
   return s
+end
+
+
+function dumpToFile(o, f, depth, codeMarker)
+  if o == nil then -- Error checking. NOTE: uses error instead of assert to be able to use the error level functionality
+    error("dumpToFile(): Must supply object to be dumped!", 2)
+
+  elseif f == nil or type(f) ~= "userdata" or f.write == nil then
+    error("dumpToFile(): Must supply a file object to dump to!", 2)
+
+  elseif type(o) == "function" or type(o) == "userdata" then
+    error("Cannot dump type: " .. type(o) .. "!", 2)
+
+  end
+  --
+  
+  local function rf(o, f, depth, codeMarker)
+    depth = depth or 0
+
+    if o == nil then 
+      return 
+
+    elseif  type(o) == "number"  then
+      f:write(o)
+
+    elseif type(o) == "boolean" then
+      f:write(o and "true" or "false") -- ASSUMTION: already checked for nil
+
+
+    elseif type(o) == "string"  then
+      if codeMarker == o:sub(1, 1) then
+        f:write(o:sub(2))
+      else
+        f:write('"' .. o .. '"')
+      end
+
+    elseif type(o) == "table" then
+      f:write("{\n")
+      depth = depth + 1
+
+      for k, v in pairs(o) do
+        local key = '["' .. k .. '"]'
+
+        if type(k) == "number" then key = "[" .. k .. "]"  
+        elseif type(k) == "string" then key = k 
+        end
+
+        f:write(string.rep("  ", depth) .. key .. " = ")
+        dumpToFile(v, f, depth, codeMarker)
+        f:write(",\n")
+      end
+
+      depth = depth - 1
+      f:write(string.rep("  ", depth) .. "}")
+    end
+  end
+
+
+  return rf(o, f, depth, codeMarker)
 end
