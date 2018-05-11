d47d69fa2422de74a91dcd7858978cd1a29b06c9
Erik <enkiigm@gmail.com>
2018-05-11 16:30:37 +0200

    Added Icon and titleText

--- Ever_End_alpha/conf.lua
+++ Ever_End_alpha/conf.lua
@@ -3,6 +3,8 @@
 function love.conf(t)
   t.version = "11.1"
   
+  t.window.title = "EverEnd"
+  t.window.icon = "assets/icon.png"
   t.window.resizable = true
   t.window.fullscreen = false
   t.window.width  = 1920
