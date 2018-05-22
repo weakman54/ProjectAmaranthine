7a139e51e93a1e29603c36cfbfbe1f61b780d5f7
Müslos <emil.kiviniemi@gmail.com>
2018-05-22 10:17:25 +0200

    combo anim flip

--- Ever_End_alpha/global_consts.lua
+++ Ever_End_alpha/global_consts.lua
@@ -4,9 +4,9 @@ DEFAULT_FRAMERATE = 12
 W, H = 1920, 1080 -- pixels
 
 
-PARRY_TRESHOLD     = 0.1 -- seconds vvv
+PARRY_TRESHOLD     = 0.2 -- amount of time that you can dodge / more time = more easy 
 PERFDODGE_TRESHOLD = 0.2
-NORMDODGE_TRESHOLD = 0.3
+NORMDODGE_TRESHOLD = 0.4
 
 
-INTENSE_DAMAGE_TRESHOLD = 3 -- HP
+INTENSE_DAMAGE_TRESHOLD = 5 -- HP 
