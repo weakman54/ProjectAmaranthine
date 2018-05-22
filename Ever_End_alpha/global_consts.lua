0d30059367c01ae11266e9949f89746461c97282
Erik <enkiigm@gmail.com>
2018-05-22 10:41:16 +0200

    Made parry timings global

--- Ever_End_alpha/global_consts.lua
+++ Ever_End_alpha/global_consts.lua
@@ -9,4 +9,14 @@ PERFDODGE_TRESHOLD = 0.2
 NORMDODGE_TRESHOLD = 0.3
 
 
+PARRY1_DURATION = 1.5     -- duration, the entire duration until the attack happens
+PARRY1_TARGET   = 1.250   -- target, the optimal time to hit the attack button
+
+PARRY2_DURATION = 1.0
+PARRY2_TARGET   = 0.865
+
+PARRY3_DURATION = 0.5
+PARRY3_TARGET   = 0.4375
+
+
 INTENSE_DAMAGE_TRESHOLD = 3 -- HP
