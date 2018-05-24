fbda1e0646be2ac5cdf956c7b4bf96623bae6b87
emoll <emilelthammar@gmail.com>
2018-05-24 14:23:05 +0200

    Scene 4_2 added boxes in code

--- Ever_End_alpha/assets/VN/sceneScript04_2.lua
+++ Ever_End_alpha/assets/VN/sceneScript04_2.lua
@@ -1,4 +1,3 @@
--- REPLACE FILE with generated script when background boxes exist
 local RM = require 'resourceManager.resourceManager'
 RM.prefix = 'assets/VN/scene04_2_killOption/'
 
@@ -15,16 +14,16 @@ return {
         },
 		music = "stop",
         anims = {
---          Box = RM:loadAnimation('s04_2_p001_m01_Box_'),
+          Box = RM:loadAnimation('s04_2_p001_m01_Box_'),
           End = RM:loadAnimation('s04_2_p001_m01_End_'),
         },
         transitionTrigger = {
           [1] = "waitForInput",
         },
         drawData = {
---          [1] = {
---            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
---          },
+          [1] = {
+            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
+          },
           [2] = {
             anim = "End",
           },
@@ -41,16 +40,16 @@ return {
         sounds = {
         },
         anims = {
---          Box = RM:loadAnimation('s04_2_p002_m01_Box_'),
+          Box = RM:loadAnimation('s04_2_p002_m01_Box_'),
           Zero = RM:loadAnimation('s04_2_p002_m01_Zero_'),
         },
         transitionTrigger = {
           [1] = "waitForInput",
         },
         drawData = {
---          [1] = {
---              anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
---            },  
+          [1] = {
+              anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
+            },  
           [2] = {
             anim = "Zero",
           },
@@ -67,16 +66,16 @@ return {
         sounds = {
         },
         anims = {
---          Box = RM:loadAnimation('s04_2_p003_m01_Box_'),
+          Box = RM:loadAnimation('s04_2_p003_m01_Box_'),
           End = RM:loadAnimation('s04_2_p003_m01_End_'),
         },
         transitionTrigger = {
           [1] = "waitForInput",
         },
         drawData = {
---          [1] = {
---            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
---          },
+          [1] = {
+            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
+          },
           [2] = {
             anim = "End",
           },
@@ -93,16 +92,16 @@ return {
         sounds = {
         },
         anims = {
---          Box = RM:loadAnimation('s04_2_p004_m01_Box_'),
+          Box = RM:loadAnimation('s04_2_p004_m01_Box_'),
           End_Zero = RM:loadAnimation('s04_2_p004_m01_End_Zero_'),
         },
         transitionTrigger = {
           [1] = "waitForInput",
         },
         drawData = {
---          [1] = {
---            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
---          },
+          [1] = {
+            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
+          },
           [2] = {
             anim = "End_Zero",
           },
@@ -112,14 +111,14 @@ return {
   },
   [5] = {
     bg = {
-      anim = RM:loadAnimation('s04_2_p005_bg_'),
+      anim = background,
     },
     moments = {
       [1] = {
         sounds = {
         },
         anims = {
---          Box = RM:loadAnimation('s04_2_p005_m01_Box_'),
+          Box = RM:loadAnimation('s04_2_p005_m01_Box_'),
           End_Zero = RM:loadAnimation('s04_2_p005_m01_End_Zero_'),
         },
         transitionTrigger = {
@@ -135,23 +134,23 @@ return {
   },
   [6] = {
     bg = {
-      anim = RM:loadAnimation('s04_2_p006_bg_'),
+      anim = background,
     },
     moments = {
       [1] = {
         sounds = {
         },
         anims = {
---          Box = RM:loadAnimation('s04_2_p006_m01_Box_'),
+          Box = RM:loadAnimation('s04_2_p006_m01_Box_'),
           End_Zero = RM:loadAnimation('s04_2_p006_m01_End_Zero_'),
         },
         transitionTrigger = {
           [1] = "waitForInput",
         },
         drawData = {
---          [1] = {
----            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
---          },
+          [1] = {
+            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
+          },
           [2] = {
             anim = "End_Zero",
           },
@@ -161,23 +160,23 @@ return {
   },
   [7] = {
     bg = {
-      anim = RM:loadAnimation('s04_2_p007_bg_'),
+      anim = background,
     },
     moments = {
       [1] = {
         sounds = {
         },
         anims = {
---          Box = RM:loadAnimation('s04_2_p007_m01_Box_'),
+          Box = RM:loadAnimation('s04_2_p007_m01_Box_'),
           Quit = RM:loadAnimation('s04_2_p007_m01_Quit_'),
         },
         transitionTrigger = {
           [1] = "waitForInput",
         },
         drawData = {
-      --          [1] = {
----            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
---          },
+                [1] = {
+            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
+          },
           [2] = {
             anim = "Quit",
           },
@@ -187,46 +186,46 @@ return {
   },
   [8] = {
     bg = {
-      anim = RM:loadAnimation('s04_2_p008_bg_'),
+      anim = background,
     },
     moments = {
       [1] = {
         sounds = {
         },
         anims = {
---          Box = RM:loadAnimation('s04_2_p001_m08_Box_'),
+          Box = RM:loadAnimation('s04_2_p008_m01_Box_'),
           
         },
         transitionTrigger = {
           [1] = "waitForInput",
         },
         drawData = {
---      [1] = {
---            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
---          },
+      [1] = {
+            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
+          },
         },
       },
     },
   },
   [9] = {
     bg = {
-      anim = RM:loadAnimation('s04_2_p009_bg_'),
+      anim = background,
     },
     moments = {
       [1] = {
         sounds = {
         },
         anims = {
---          Box = RM:loadAnimation('s04_2_p009_m01_Box_'),
+          Box = RM:loadAnimation('s04_2_p009_m01_Box_'),
         },
         transitionTrigger = {
           [1] = "waitForInput",
           gotoScene = "End",
         },
         drawData = {
---          [1] = {
---            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
---          },
+          [1] = {
+            anim = "Box",alpha=boxAlpha,red=boxDarken,green=boxDarken,blue=boxDarken
+          },
         },
       },
     },
