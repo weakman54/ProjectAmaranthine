73a4accdd3f282bb1744260dc1c159a38f1c3f0e
Müslos <emil.kiviniemi@gmail.com>
2018-05-25 14:52:50 +0200

    Scene 4 textboxes with text

--- Ever_End_alpha/gamestates/stateVN.lua
+++ Ever_End_alpha/gamestates/stateVN.lua
@@ -4,8 +4,8 @@ local VNSystem = reload("VNSystem")
 local stateVN = {}
 
 
-local sceneToLoad = "04_2"
-local panelToLoad = 2
+local sceneToLoad = "03_0"
+local panelToLoad = 1
 
 function stateVN:init()
 
