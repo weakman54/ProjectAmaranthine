3047006e88b7e09be6e20128f2c2e1d567492a75
The one guy <poppyonetrick@gmail.com>
2018-05-25 14:20:49 +0200

    Added Enemy Telegraph in 3rd Fight

--- Ever_End_alpha/enemyQuit3.lua
+++ Ever_End_alpha/enemyQuit3.lua
@@ -253,6 +253,7 @@ function enemy:initializeSM()
   sm:add("offensive", {
       enter = function(self, kind, nextAttack)
         enemy.dbg_trigger_offensive_action = false
+		Sound:play("Telegraph", {delay = 0.5})
 
 
         -- TODO: choose action #
