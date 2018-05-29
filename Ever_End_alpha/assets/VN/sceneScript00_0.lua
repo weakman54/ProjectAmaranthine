eb6f3c0507b51ae113998bcf8e450189820cd324
Erik <enkiigm@gmail.com>
2018-05-29 11:15:35 +0200

    Fixed Scene 00_0 intro text

---
+++ Ever_End_alpha/assets/VN/sceneScript00_0.lua
@@ -0,0 +1,97 @@
+
+local RM = require 'resourceManager.resourceManager'
+RM.prefix = 'assets/VN/scene00_0_recap/'
+
+return {
+  [1] = {
+    bg = {
+      anim = RM:loadAnimation('s00_0_p001_m01_Recap_'),
+    },
+    moments = {
+      [1] = {
+        sounds = {
+        },
+        anims = {
+        },
+        transitionTrigger = {
+          [1] = "waitForInput",
+        },
+        drawData = {
+        },
+      },
+    },
+  },
+  [2] = {
+    bg = {
+      anim = RM:loadAnimation('s00_0_p002_m01_Recap_'),
+    },
+    moments = {
+      [1] = {
+        sounds = {
+        },
+        anims = {
+        },
+        transitionTrigger = {
+          [1] = "waitForInput",
+        },
+        drawData = {
+        },
+      },
+    },
+  },
+  [3] = {
+    bg = {
+      anim = RM:loadAnimation('s00_0_p003_m01_Recap_'),
+    },
+    moments = {
+      [1] = {
+        sounds = {
+        },
+        anims = {
+        },
+        transitionTrigger = {
+          [1] = "waitForInput",
+        },
+        drawData = {
+        },
+      },
+    },
+  },
+  [4] = {
+    bg = {
+      anim = RM:loadAnimation('s00_0_p004_m01_Recap_'),
+    },
+    moments = {
+      [1] = {
+        sounds = {
+        },
+        anims = {
+        },
+        transitionTrigger = {
+          [1] = "waitForInput",
+        },
+        drawData = {
+        },
+      },
+    },
+  },
+  [5] = {
+    bg = {
+      anim = RM:loadAnimation('s00_0_p005_m01_Recap_'),
+    },
+    moments = {
+      [1] = {
+        sounds = {
+        },
+        anims = {
+        },
+        transitionTrigger = {
+          [1] = "waitForInput",
+          gotoScene = "01_0",
+        },
+        drawData = {
+        },
+      },
+    },
+  },
+}
