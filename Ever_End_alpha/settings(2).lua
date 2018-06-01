816d08c3da2bf677747a7e1627e7a5ce1dccaf0e
The one guy <poppyonetrick@gmail.com>
2018-06-01 16:11:54 +0200

    ugh settings 2

--- Ever_End_alpha/settings(2).lua
+++ Ever_End_alpha/settings(2).lua
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream:Ever_End_alpha/settings(2).lua
 -- TODO: make into table
 vibrationEnabled = false
 
 sceneToLoad = "01_0"
 panelToLoad = 15
+=======
+-- TODO: make into table
+vibrationEnabled = false
+
+sceneToLoad = "03_0"
+panelToLoad = 1
+>>>>>>> Stashed changes:Ever_End_alpha/settings.lua
