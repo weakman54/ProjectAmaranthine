3398cf811f2b6e1228565df006e4a6fc281f0293
Erik <enkiigm@gmail.com>
2018-06-28 13:33:07 +0200

    Added configuration files and changed relevant places to run from config

--- Ever_End_alpha/conf.lua
+++ Ever_End_alpha/conf.lua
@@ -1,12 +1,23 @@
 
+local c = require "configuration"
+
+if c.vsync then c.vsync = 1 else c.vsync = 0 end
+if c.fullscreen == nil then c.fullscreen = true end
 
 function love.conf(t)
   t.version = "11.1"
-  
+
   t.window.title = "EverEnd"
   t.window.icon = "assets/icon.png"
   t.window.resizable = true
-  t.window.fullscreen = false
-  t.window.width  = 1920
-  t.window.height = 1080
+
+  t.window.width  = c.windowWidth  or 1920
+  t.window.height = c.windowHeight or 1080
+
+  t.window.fullscreen = c.fullscreen -- Set above
+  t.window.fullscreentype  = c.fullscreentype or "desktop"
+--  t.window.borderless = c.borderless or false (This seem to fuck with the scaling for some reason...
+
+  t.window.vsync = c.vsync -- Set above
+  t.window.msaa  = c.msaa  or 2
 end
